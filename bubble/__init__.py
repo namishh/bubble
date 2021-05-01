2c399b104da4b1214753ab2e63bc53aac548b8a4
dark-Jedi2108 <68964499+dark-Jedi2108@users.noreply.github.com>
2021-05-01 13:13:14 +0530

    Update __init__.py

--- bubble/__init__.py
+++ bubble/__init__.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SECRET_KEY'] = 'wearsgjytytwaw56425345y65ytyrdfgthyetfgrseths'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
